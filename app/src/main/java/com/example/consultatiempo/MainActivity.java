e57ccb24d3432241ec127f8a7b729c29be230dd8
Jun Zhou <zhoujun97099@gmail.com>
2023-02-17 00:51:41 +0100

    finished basic functionalities

--- app/src/main/java/com/example/consultatiempo/MainActivity.java
+++ app/src/main/java/com/example/consultatiempo/MainActivity.java
@@ -2,24 +2,41 @@ package com.example.consultatiempo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText longitud;
-    EditText latitud;
-    Button consultar;
+    EditText etLongitud;
+    EditText etLatitud;
+    Button btnConsultar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        longitud = findViewById(R.id.editTextLongitud);
-        latitud = findViewById(R.id.editTextLatitud);
-        consultar = findViewById(R.id.btnConsulta);
+        etLongitud = findViewById(R.id.editTextLongitud);
+        etLatitud = findViewById(R.id.editTextLatitud);
+        btnConsultar = findViewById(R.id.btnConsulta);
+        btnConsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+                    intent.putExtra("longitud", Double.parseDouble(etLongitud.getText().toString()));
+                    intent.putExtra("latitud", Double.parseDouble(etLatitud.getText().toString()));
+                    startActivity(intent);
 
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Error en los datos", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
     }
 }
