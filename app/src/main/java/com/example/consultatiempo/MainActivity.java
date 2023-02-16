8fd643f665fb66392cca239c5db395910b0e3715
Jun Zhou <zhoujun97099@gmail.com>
2023-02-16 12:06:46 +0100

    finished UI design

--- app/src/main/java/com/example/consultatiempo/MainActivity.java
+++ app/src/main/java/com/example/consultatiempo/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.consultatiempo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText longitud;
+    EditText latitud;
+    Button consultar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        longitud = findViewById(R.id.editTextLongitud);
+        latitud = findViewById(R.id.editTextLatitud);
+        consultar = findViewById(R.id.btnConsulta);
+
+
     }
 }
